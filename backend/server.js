9792fae330945e4f492e0d6d12a1739ee63a6c55
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-05-13 23:10:43 +0530

    Error & Exception Handling

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const PORT = process.env.PORT || 8000
 const app = express()
 
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
-  res.status(200).json({ msg: `Welcome to the Support Desk API` })
+  res.status(200).json({ message: `Welcome to the Support Desk API` })
 })
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Custom Middleware
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
