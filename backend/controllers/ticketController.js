d2a13a36687fc929b78eda4a83531056ab35a643
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-05-14 22:37:56 +0530

    Ticket Model & Routes

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,22 @@
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+const Ticket = require('../models/ticketModel')
+
+// @desc    Get user tickets
+// @route   /api/tickets
+// @access  Private
+const getTickets = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get Tickets' })
+})
+
+// @desc    Create new ticket
+// @route   /api/tickets
+// @access  Private
+const createTicket = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Create Ticket' })
+})
+
+module.exports = {
+  getTickets,
+  createTicket,
+}
